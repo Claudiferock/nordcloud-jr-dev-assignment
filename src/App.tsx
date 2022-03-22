87b53ddba13a8dce544aeff68b54e2898beeebef
Claudiferock <claudio.rodriguez.vasquez@myy.haaga-helia.fi>
2022-03-22 16:12:21 +0200

    Update work
    Add extra state and useEffect
    Includes some bugs to be fixed soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NetworkStationsList from './modules/NetworkStationsList';
 import DevicesList from './modules/DevicesList';
-import { Network, Device } from './types/global';
+import { TNetwork, TDevice } from './types/global';
+import { getOptimalNetwork, showOutput } from './utilities/utils';
 import './App.css';
 
 function App() {
-  const [selectedDevice, setSelectedDevice] = useState <null | Device> (null);
-  const [bestStation, setBestStation] = useState <null | Network> (null);
+  const [selectedDevice, setSelectedDevice] = useState <null | TDevice> (null);
+  const [bestStation, setBestStation] = useState <null | TNetwork> (null);
+  const [output, setOutput] = useState <string> ('');
+
+  useEffect(() => {
+    selectedDevice && setBestStation(getOptimalNetwork(selectedDevice));
+    console.log('Extra test:', getOptimalNetwork([100,100]))
+  }, [selectedDevice]);
+
+  useEffect(() => {
+    output = showOutput(selectedDevice, bestStation)
+  }, [bestStation]);
+
+  const selectDevice = function(device: null | TDevice): void {
+    setSelectedDevice(device);
+  }
+
 
   return (
     <div className="App">
@@ -14,10 +30,13 @@ function App() {
         <h1>Find the optimal Network Station for your Device</h1>
       </header>
       <main>
+      {selectedDevice && <p>{ showOutput(selectedDevice, bestStation) }</p>}
+        <DevicesList
+          selectDevice={ selectDevice }
+        />
         <NetworkStationsList
           
         />
-        <DevicesList/>
       </main>
     </div>
   );
