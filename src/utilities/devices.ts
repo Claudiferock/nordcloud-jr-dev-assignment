f5376e14b048084497050c91bf2cc2902469f464
Claudiferock <claudio.rodriguez.vasquez@myy.haaga-helia.fi>
2022-03-22 16:08:43 +0200

    Update imported types names

--- src/utilities/devices.ts
+++ src/utilities/devices.ts
@@ -1,2 +1,2 @@
-import { Device } from '../types/global';
-export const DEVICES : ReadonlyArray<Device> = [[0,0], [100,100], [15,10], [18,18], [13,13], [25,99]];
+import { TDevice } from '../types/global';
+export const DEVICES : ReadonlyArray<TDevice> = [[0,0], [100,100], [15,10], [18,18], [13,13], [25,99]];
--- src/utilities/networkStations.ts
+++ src/utilities/networkStations.ts
@@ -1,3 +1,3 @@
-import { Network } from '../types/global';
+import { TNetwork } from '../types/global';
 
-export const NETWORKSTATIONS : ReadonlyArray<Network> = [[0,0,9], [20,20,6], [10,0,12], [5,5,13], [99,25,2]];
+export const NETWORKSTATIONS : ReadonlyArray<TNetwork> = [[0,0,9], [20,20,6], [10,0,12], [5,5,13], [99,25,2]];
