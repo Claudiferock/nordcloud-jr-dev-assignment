882c3ad737f67a6b27f525eddaebc7130c054683
Claudiferock <claudio.rodriguez.vasquez@myy.haaga-helia.fi>
2022-03-22 16:08:06 +0200

    Add getOptimalNetwork and showOutput functions

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -1,4 +1,5 @@
-import { Network, Device} from '../types/global';
+import { TNetwork, TDevice, TNetworkState, TDeviceState} from '../types/global';
+import { NETWORKSTATIONS } from './networkStations';
 
 export const uid = function (): string {
   const head = Date.now().toString(36);
@@ -7,22 +8,40 @@ export const uid = function (): string {
   return head + tail;
 }
 
-export const getSpeed = function (network: Network, deviceDistance: number): number {
+export const getSpeed = function (network: TNetwork, deviceDistance: number): number {
   const networkReach = network[2];
-  //const [networkX, networkY, networkReach] = network;
-  //const [deviceX, deviceY] = device;
-  //const deviceDistance = getDistance(networkX, deviceX, networkY, deviceY);
   const networkSpeed = deviceDistance > networkReach ? 0 : Math.pow((networkReach - deviceDistance), 2);
 
   return networkSpeed;
 }
 
-export const getDistance = function (network: Network, device: Device): number {
-  // d is the distance between two points (coordinates) in space 
+export const getDistance = function (network: TNetwork, device: TDevice): number {
+  // distance between two points (coordinates) in space 
   // d = sqrt((x2 - x1)^2 + (y2 - y1)^2)
   const [x1, y1] = device;
   const [x2, y2] = network;
   const distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
   
   return distance;
+}
+
+export const getOptimalNetwork = function (device: TDeviceState): TNetworkState {
+  let bestSpeed: number = 0;
+  let bestNetworkStation: TNetworkState = null;
+  device && NETWORKSTATIONS.forEach(network => {
+    const distance = getDistance(network, device);
+    const speed = getSpeed(network, distance);
+    if (speed > bestSpeed) {
+      bestSpeed = speed;
+      bestNetworkStation = network;
+    };
+  });
+
+  return bestNetworkStation;
+}
+
+export const showOutput = function (device: TDevice, network: TNetworkState): string {
+  const outOfReach = `No network station within reach for point ${device[0]},${device[1]}`;
+  const withinReach = `Best network station for point ${device[0]},${device[1]} is ${network[0]},${network[1]} with speed ${network[2]}`
+  return network === null ? outOfReach : withinReach;
 }
